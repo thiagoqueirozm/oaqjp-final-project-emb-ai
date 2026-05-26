7d2fb8f746198473902a9f66047f21138d09056d
Thiago Santos <thiagoqmeneses@hotmail.com>
2026-05-26 11:21:53 -0400

    Create EmotionDetection package

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,7 +2,6 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
     headers = {
@@ -17,18 +16,9 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json=input_json, headers=headers)
 
-    if response.status_code == 400:
-        return {
-            'anger': None,
-            'disgust': None,
-            'fear': None,
-            'joy': None,
-            'sadness': None,
-            'dominant_emotion': None
-        }
-
     formatted_response = json.loads(response.text)
     emotions = formatted_response['emotionPredictions'][0]['emotion']
+
     dominant_emotion = max(emotions, key=emotions.get)
 
     return {
